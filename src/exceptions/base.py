75425e0247b161cec7980ae5227923a00fea25ff
israel_farkash <140426593+ASDFG0537701349@users.noreply.github.com>
2026-02-21 21:46:05 +0200

    Revert "refactor: architecture overhaul + image bug fix + Docker"

--- src/exceptions/base.py
+++
@@ -1,2 +0,0 @@
-class BotBaseError(Exception):
-    pass
--- src/exceptions/telegram_errors.py
+++
@@ -1,9 +0,0 @@
-from src.exceptions.base import BotBaseError
-
-
-class TelegramClientError(BotBaseError):
-    pass
-
-
-class TelegramFileRetrievalError(TelegramClientError):
-    pass
